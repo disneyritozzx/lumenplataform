0f024e8385e3f98285fd6d14acc6e3b2d33aa243
lumenrps <joaoaoaoa158@gmail.com>
2024-11-17 19:56:30 -0300

    i removed every game due to high memory use
    i will try other way to dont use that many mem in the repo(also thanks to sz games to have a public repo! I really appreciate it)

--- games/THE 14TH AXE WOUND/js/plugins/Followers.js
+++
@@ -1,7 +0,0 @@
-Game_Player.prototype.isCollided = function(x, y) {
-    if (this.isThrough()) {
-        return false;
-    } else {
-        return this.pos(x, y);
-    }
-};
--- games/whatthat/README.md
+++
@@ -1,4 +0,0 @@
-# Fly-Hydra
-I made this to learn PixiJS
-
-This was made for <b>personal use only.</b>
